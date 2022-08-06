cd6509e8d10582b53b60371293d05fec80be0f26
sedatcakir26 <sedatcakir@gmail.com>
2022-08-06 17:40:50 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Sedat Çakır on 6.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
